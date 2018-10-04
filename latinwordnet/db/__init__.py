24c636003e6c00b8a6f03454c69e553acf3b89aa
Surkal <user@server.fake>
2018-10-04 21:13:52 +0200

    Add linux compatibility

--- latinwordnet/db/__init__.py
+++ latinwordnet/db/__init__.py
@@ -2,19 +2,22 @@
 
 import codecs
 import os
+import sys
 import sqlite3
 from os import listdir
 from sqlite3 import OperationalError
 
 from tqdm import tqdm
 
+module = sys.modules['latinwordnet.db'].__path__[0]
+
 
 def connect(language, database):
     """ Connects to a database """
 
     try:
-        if os.path.exists(f"../latinwordnet/latinwordnet/db/{language}/{language}_{database}.db"):
-            cursor = sqlite3.connect(f"../latinwordnet/latinwordnet/db/{language}/{language}_{database}.db").cursor()
+        if os.path.exists(f"{module}/{language}/{language}_{database}.db"):
+            cursor = sqlite3.connect(f"{module}/{language}/{language}_{database}.db").cursor()
         else:
             raise OperationalError
     except OperationalError:
@@ -24,16 +27,16 @@ def connect(language, database):
 
 def compile(language, *tables):
     if not tables:
-        tables = [filename.split('_')[1].replace('.sql', '') for filename in listdir(f"../latinwordnet/latinwordnet/db/{language}/") if filename.endswith('.sql')]
+        tables = [filename.split('_')[1].replace('.sql', '') for filename in listdir(f"{module}/{language}/") if filename.endswith('.sql')]
 
     for table in tables:
-        f = codecs.open(f"../latinwordnet/latinwordnet/db/{language}/{language}_{table}.sql", encoding='utf-8')
-        if f:
-            db = sqlite3.connect(f"../latinwordnet/latinwordnet/db/{language}/{language}_{table}.db").cursor()
+        f = codecs.open(f"{module}/{language}/{language}_{table}.sql", encoding='utf-8')
+        if not f:
+            continue
+        db = sqlite3.connect(f"{module}/{language}/{language}_{table}.db").cursor()
 
-            if db:
-                for sql in tqdm(f.readlines(), ncols=80, desc=f"{language}_{table}.sql"):
-                    if sql.startswith('#') or sql == '\n':
-                        continue
-                    else:
-                        db.executescript(sql)
+        if not db:
+            continue
+        for sql in tqdm(f.readlines(), ncols=80, desc=f"{language}_{table}.sql"):
+            if not(sql.startswith('#') or sql == '\n'):
+                db.executescript(sql)
